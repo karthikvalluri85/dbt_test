a695324f134a09b39952b8d99287c0b4ba7a0825
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-13 12:50:00 +0530

    Automated DBT Sync

--- models/bronze/bz_support_tickets.sql
+++ models/bronze/bz_support_tickets.sql
@@ -20,7 +20,7 @@ with raw_support_tickets as (
         load_timestamp as raw_load_timestamp,
         update_timestamp as raw_update_timestamp,
         source_system as raw_source_system
-    from {{ source('zoom_raw', 'support_tickets') }}
+    from {{ source('zoom_raw_schema', 'support_tickets') }}
     where ticket_id is not null  -- Basic data quality check
 ),
 
