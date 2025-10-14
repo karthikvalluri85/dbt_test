609f29ad333fffd0f62bcce4e048cf4c521ce1b9
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-14 10:49:34 +0530

    Automated DBT Sync

---
+++ models/bronze/bz_meetings.sql
@@ -0,0 +1,50 @@
+{{ config(
+    materialized='table',
+    pre_hook="{% if this.name != 'bz_audit_log' %}INSERT INTO {{ ref('bz_audit_log') }} (SOURCE_TABLE, LOAD_TIMESTAMP, PROCESSED_BY, PROCESSING_TIME, STATUS) VALUES ('BZ_MEETINGS', CURRENT_TIMESTAMP(), 'DBT_PROCESS', 0, 'STARTED'){% endif %}",
+    post_hook="{% if this.name != 'bz_audit_log' %}INSERT INTO {{ ref('bz_audit_log') }} (SOURCE_TABLE, LOAD_TIMESTAMP, PROCESSED_BY, PROCESSING_TIME, STATUS) VALUES ('BZ_MEETINGS', CURRENT_TIMESTAMP(), 'DBT_PROCESS', DATEDIFF('second', (SELECT MAX(LOAD_TIMESTAMP) FROM {{ ref('bz_audit_log') }} WHERE SOURCE_TABLE = 'BZ_MEETINGS' AND STATUS = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED'){% endif %}"
+) }}
+
+-- Bronze Layer Meetings Table
+-- Raw to Bronze 1:1 mapping for Zoom Meetings data
+-- Author: DBT Data Engineering Team
+-- Created: {{ run_started_at }}
+
+WITH source_data AS (
+    SELECT 
+        -- Direct 1:1 mapping from raw to bronze
+        MEETING_ID,
+        MEETING_TOPIC,
+        HOST_ID,
+        START_TIME,
+        END_TIME,
+        DURATION_MINUTES,
+        LOAD_TIMESTAMP,
+        UPDATE_TIMESTAMP,
+        SOURCE_SYSTEM
+    FROM {{ source('zoom_raw', 'meetings') }}
+),
+
+data_quality_checks AS (
+    SELECT 
+        *,
+        -- Add data quality indicators
+        CASE 
+            WHEN MEETING_ID IS NULL THEN 'MISSING_ID'
+            WHEN START_TIME > END_TIME THEN 'INVALID_TIME_RANGE'
+            ELSE 'VALID'
+        END AS data_quality_status
+    FROM source_data
+)
+
+SELECT 
+    MEETING_ID,
+    MEETING_TOPIC,
+    HOST_ID,
+    START_TIME,
+    END_TIME,
+    DURATION_MINUTES,
+    LOAD_TIMESTAMP,
+    UPDATE_TIMESTAMP,
+    SOURCE_SYSTEM
+FROM data_quality_checks
+-- Include all records, even those with quality issues for bronze layer
