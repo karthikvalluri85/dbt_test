36dbd0d442299563621519038c74c75735441199
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-21 17:59:32 +0530

    Automated DBT Sync

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -28,7 +28,7 @@ WITH source_data AS (
         source_system,
         load_timestamp,
         update_timestamp
-    FROM {{ source('zoom_raw', 'users') }}
+    FROM {{ source('zoom_raw_schema', 'users') }}
     WHERE user_id IS NOT NULL  -- Basic data quality check
 ),
 
